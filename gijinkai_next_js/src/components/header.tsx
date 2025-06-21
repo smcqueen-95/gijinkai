b687891ef844be26e6798105b347dcfd4b2d4d09
GuiTheDevv <104726381+GuiTheDevv@users.noreply.github.com>
2025-06-20 22:16:44 -0400

    innit

---
+++ gijinkai_next_js/src/components/header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="bg-white shadow-sm">
+      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold text-gray-900">
+          <span className="block">Gijinkai</span>
+          <span className="block text-indigo-600">Next.js</span>
+        </h1>
+      </div>
+    </header>
+  );
+}
